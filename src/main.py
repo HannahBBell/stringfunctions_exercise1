4c52836c6567d0619b8917190935b6518ec849eb
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-01-27 17:05:50 +0000

    reverse string function added

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
 def capitalise_string(s):
     return s
+
+def decapitalise_string(st):
+    return s.lower()
+
+def first_letter_capitalisation(word):
+    return word.title()
+
+def reverse_string(string):
+    return string[::-1]
